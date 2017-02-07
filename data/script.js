3f90e97e820e149be62054c74079f8750bca7481
Mathieu Leplatre <mathieu@mozilla.com>
2017-02-07 18:01:20 +0100

    Show local info

--- data/script.js
+++ data/script.js
@@ -1,6 +1,8 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
 const {Preferences} = Cu.import("resource://gre/modules/Preferences.jsm", {});
+const {Kinto} = Cu.import("resource://services-common/kinto-offline-client.js", {});
+const {FirefoxAdapter} = Cu.import("resource://services-common/kinto-storage-adapter.js", {});
 
 
 function main() {
@@ -9,9 +11,15 @@ function main() {
 
   // Poll for changes button.
   const updater = Cu.import("resource://services-common/blocklist-updater.js", {});
-  const pollButton = document.getElementById("run-poll").onclick = () => {
+  document.getElementById("run-poll").onclick = () => {
     updater.checkVersions()
       .then(showPollingStatus);
+  }
+
+  // Reset local data.
+  document.getElementById("clear-data").onclick = () => {
+    Preferences.reset("services.blocklist.last_update_seconds");
+    Preferences.reset("services.blocklist.last_etag");
   }
 }
 
@@ -24,8 +32,7 @@ function showPollingStatus() {
     { target: "changespath",    name: "services.blocklist.changes.path" },
     { target: "last-poll",      name: "services.blocklist.last_update_seconds" },
     { target: "timestamp",      name: "services.blocklist.last_etag" },
-    { target: "humantimestamp", name: "services.blocklist.last_etag" },
-    { target: "clockskew",      name: "services.blocklist.clock_skew_seconds"}
+    { target: "clockskew",      name: "services.blocklist.clock_skew_seconds" }
   ];
 
   for(const pref of prefs) {
@@ -36,7 +43,7 @@ function showPollingStatus() {
       case "last-poll":
         value = new Date(parseInt(value, 10) * 1000);
         break;
-      case "humantimestamp":
+      case "timestamp":
         value = new Date(parseInt(value.replace('"', ''), 10));
         break;
     }
@@ -69,6 +76,7 @@ function showBlocklistStatus() {
   const statusList = document.getElementById("blocklists-status");
   statusList.innerHTML = "";
 
+
   const collections = ["addons", "onecrl", "plugins", "gfx", "pinning"];
   collections.forEach((collection) => {
     const bucket = collection == "pinning" ? pinningBucket : blocklistBucket;
@@ -77,12 +85,46 @@ function showBlocklistStatus() {
     const lastCheckedSeconds = Preferences.get(`services.blocklist.${collection}.checked`);
     const lastChecked = new Date(parseInt(lastCheckedSeconds, 10) * 1000);
 
-    const infos = tpl.content.cloneNode(true);
-    infos.querySelector(".blocklist").textContent = collection;
-    infos.querySelector(".url").textContent = url;
-    infos.querySelector(".last-check").textContent = lastChecked;
-    statusList.appendChild(infos);
+    fetchLocal(bucket, collectionId)
+      .then((local) => {
+        const {timestamp, records} = local;
+
+        const infos = tpl.content.cloneNode(true);
+        infos.querySelector(".blocklist").textContent = collection;
+        infos.querySelector(".url").textContent = url;
+        infos.querySelector(".timestamp").textContent = new Date(timestamp);
+        infos.querySelector(".nb-records").textContent = records.length;
+        infos.querySelector(".last-check").textContent = lastChecked;
+        statusList.appendChild(infos);
+      });
+
   });
+}
+
+
+function fetchLocal(bucket, collection) {
+  // XXX: simplify this using await/async
+
+  const server = "http://unused/v1";
+  const path = "kinto.sqlite";
+  const config = {remote: server, adapter: FirefoxAdapter, bucket};
+
+  return FirefoxAdapter.openConnection({path})
+    .then((sqliteHandle) => {
+      const options = Object.assign({}, config, {adapterOptions: {sqliteHandle}})
+      const localCollection = new Kinto(options).collection(collection);
+
+      return localCollection.db.getLastModified()
+        .then((timestamp) => {
+          return localCollection.list()
+            .then(({data: records}) => {
+              return sqliteHandle.close()
+                .then(() => {
+                  return {timestamp, records};
+                });
+            })
+        });
+    });
 }
 
 
