23de52dd2707a6757dce5e03be4697e5d72e3672
Alex Cottner <acottner@mozilla.com>
2024-05-17 08:54:38 -0600

    Cleaning up code a bit.

--- tests/selenium.spec.js
+++ tests/selenium.spec.js
@@ -3,14 +3,17 @@ const { Options } = require('selenium-webdriver/firefox');
 const FirefoxProfile = require('firefox-profile');
 const path = require('path');
 
+// create a static extension ID so we can find it's config page easily
 const testExtId = "2d7fbdec-9526-402c-badb-2fca5b65dfa8";
 let driver = null;
 
 beforeAll(async() => {
+  // create a firefox profile that has our extension added to it
   const xpiPath = path.resolve('./web-ext-artifacts/remote-settings-devtools.xpi');
   let profile = new FirefoxProfile()
-  profile.addExtension(xpiPath, (err, details) => { });
+  profile.addExtension(xpiPath, (err, details) => { }); // empty function is required to load
 
+  // setup firefox options that will allow our extension to run
   const options = new Options(profile.path());
   options.setBinary(process.env.NIGHTLY_PATH || "/usr/bin/firefox-nightly");
   options.addArguments("--pref 'extensions.experiments.enabled=true'");
@@ -26,6 +29,7 @@ beforeAll(async() => {
     .setFirefoxOptions(options)
     .build();
 
+  // install the addon
   await driver.installAddon(xpiPath);
   await driver.get(`moz-extension://${testExtId}/content/index.html`);
 });
@@ -35,20 +39,24 @@ afterAll(async() => {
   driver.close();
 });
 
-
+// helper function to wait while data is being fetched
 async function waitForLoad() {
-  let body = await driver.findElement(By.css("body"));
-  while (await body.getAttribute('class') !== '') {
+  do {
     driver.sleep(200);
-  }
+  } while((await driver.findElements(By.css('.loading'))).length);
 }
 
+// making this a little easier to read in tests
+async function clickByCss(css) {
+  let element = await driver.findElement(By.css(css));
+  await element.click();
+}
 
 describe("End to end browser tests", () => {
-  test("Load extension, change environment to prod, and sync", async() => {
-    let ddlEnvironment = await driver.findElement(By.id("environment"));
-    await ddlEnvironment.click();
-    await (await ddlEnvironment.findElement(By.css('[value="prod"]'))).click();
+  test("Load extension, change environment to prod, sync and clear all", async() => {
+    // select prod environment from dropdown
+    await clickByCss('#environment');
+    await clickByCss('#environment [value="prod"]');
     await waitForLoad();
     
     // verify table loads as expected and we have unsync'd data
@@ -56,15 +64,33 @@ describe("End to end browser tests", () => {
     expect((await driver.findElements(By.css('#status .unsync'))).length).toBeGreaterThan(1);
 
     // pull latest data
-    await (await driver.findElement(By.id("run-poll"))).click();
+    await clickByCss('#run-poll');
     await waitForLoad();
     
     // verify data as sync'd as expected
     expect((await driver.findElements(By.css('#status .unsync'))).length).toBe(0);
     expect((await driver.findElements(By.css('#status .up-to-date'))).length).toBeGreaterThan(1);
+
+    // clear all data
+    await clickByCss('#clear-all-data');
+    await driver.sleep(1000); // wait for all collections to be cleared
+    
+    // verify everything is cleared as expected
+    expect((await driver.findElements(By.css('#status .unsync'))).length).toBeGreaterThan(1);
+    expect((await driver.findElements(By.css('#status .up-to-date'))).length).toBe(0);
   });
 
   test("Clear and re-download a collection", async() => {
-    // to do
+    // force sync the first collection and verify it worked
+    await clickByCss('#status .sync');
+    await waitForLoad();
+    let firstTimestamp = await driver.findElement(By.css('#status .human-local-timestamp'));
+    expect(await firstTimestamp.getAttribute('class')).toContain('up-to-date');
+
+    // force sync the first collection and verify it worked
+    await clickByCss('#status .clear-data');
+    await waitForLoad();
+    firstTimestamp = await driver.findElement(By.css('#status .human-local-timestamp'));
+    expect(await firstTimestamp.getAttribute('class')).toContain('unsync');
   });
 });
